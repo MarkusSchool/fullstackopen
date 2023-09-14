a992370594872cfdb1a469ba269ac1cb0b30f9d5
Markus <125263481+MarkusSchool@users.noreply.github.com>
2023-09-14 07:02:20 +0300

    alku

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,1 +1,19 @@
+import { useState } from 'react'
 
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button HandleClick={() => setGood(good + 1)}>good</button>
+      <button HandleClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button HandleClick={() => setBad(bad + 1)}>bad</button>
+    </div>
+  )
+}
+
+export default App
