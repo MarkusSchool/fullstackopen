cdda0d776ca26334ad336cf6ab9cd70b1205eb87
Markus <125263481+MarkusSchool@users.noreply.github.com>
2023-09-14 07:28:46 +0300

    done i think

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,18 +1,41 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const Statistics = (props) => {
-
-}
+  if (!props.total) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={props.good} />
+            <StatisticLine text="Neutral" value={props.neutral} />
+            <StatisticLine text="Bad" value={props.bad} />
+            <StatisticLine text="All" value={props.all} />
+            <StatisticLine text="Average" value={props.avg} />
+            <StatisticLine text="Positive" value={props.positive} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
 
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
-)
+);
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
-  )
-}
+    <p>
+      {props.text} {props.value}
+    </p>
+  );
+};
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -20,22 +43,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const AddAll = () => bad + neutral + good
-  
-  const GetAverage = () => good - bad / AddAll();
+  const AddAll = () => bad + neutral + good;
 
   return (
     <div>
-
       <h1>Give feedback</h1>
-
-      <Button HandleClick={() => setGood(good + 1)} text="Good"/>
-      <Button HandleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
-      <Button HandleClick={() => setBad(bad + 1)} text="Bad"/>
-
-
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <h1>Statistics</h1>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={AddAll()}
+        positive={(good / AddAll()) * 100}
+        avg={(good - bad) / AddAll()}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
