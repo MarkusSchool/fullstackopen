f0d2ebdb34a74bfbf14a617966b29e29138c8d2b
Markus <125263481+MarkusSchool@users.noreply.github.com>
2023-09-14 07:13:17 +0300

    progress

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,17 +1,34 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const AddAll = () => bad + neutral + good
+  
+  const GetAverage = () => good - bad / AddAll();
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button HandleClick={() => setGood(good + 1)}>good</button>
-      <button HandleClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button HandleClick={() => setBad(bad + 1)}>bad</button>
+
+      <h1>Give feedback</h1>
+
+      <Button HandleClick={() => setGood(good + 1)} text="Good"/>
+      <Button HandleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
+      <Button HandleClick={() => setBad(bad + 1)} text="Bad"/>
+
+      <h1>Statistics</h1>
+
     </div>
   )
 }
