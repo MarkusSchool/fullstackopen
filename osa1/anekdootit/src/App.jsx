0e7ed18ec2870ce6d94a7b56b98ff00eedd85ad6
Markus <125263481+MarkusSchool@users.noreply.github.com>
2023-09-14 16:05:34 +0300

    valmistettu

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -1,4 +1,10 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Votes = ({ votes }) => <p>has {votes} votes</p>;
 
 const App = () => {
   const anecdotes = [
@@ -8,17 +14,43 @@ const App = () => {
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+    'Programming without an extremely heavy use of console.log is the same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast is to go well.'
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const getRandomAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const getMostVotedAnecdote = () => {
+    const maxVotes = Math.max(...votes);
+    const indexOfMaxVotes = votes.indexOf(maxVotes);
+    return anecdotes[indexOfMaxVotes];
+  };
+
+  const vote = () => {
+    const kopio = [...votes];
+    kopio[selected]++;
+    setVotes(kopio);
+  };
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <Votes votes={votes[selected]} />
+      <div>
+        <Button handleClick={vote} text="vote" />
+        <Button handleClick={getRandomAnecdote} text="next anecdote" />
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <p>{getMostVotedAnecdote()}</p>
+      <Votes votes={votes[votes.indexOf(Math.max(...votes))]} />
     </div>
-  )
-}
- 
-export default App
+  );
+};
+
+export default App;
