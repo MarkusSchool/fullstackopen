445e7c9fcd4620db61ce0a17e4f3d94e5cfce735
renzyh <nokelainennmarkus@gmail.com>
2023-09-15 20:32:24 +0300

    making Content

--- osa2/kurssitiedot/src/Course.jsx
+++ osa2/kurssitiedot/src/Course.jsx
@@ -4,11 +4,16 @@ const Header = ({coursename}) => {
     )
 }
 
+const Content = ({courseparts}) => {
+    console.log(courseparts)
+}
+
 const Course = ({ course }) => {
     console.log(course.name)
     return (
         <div>
             <Header coursename={course.name}/>
+            <Content courseparts={course.parts}/>
         </div>
     )
 }
