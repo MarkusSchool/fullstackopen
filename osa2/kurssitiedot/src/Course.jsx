57dbee0d1a520511e139b9bc140b4b367470e483
renzyh <nokelainennmarkus@gmail.com>
2023-09-15 20:44:41 +0300

    making Content

--- osa2/kurssitiedot/src/Course.jsx
+++ osa2/kurssitiedot/src/Course.jsx
@@ -1,19 +1,40 @@
-const Header = ({coursename}) => {
+const Header = ({ coursename }) => {
     return (
         <h1>{coursename}</h1>
     )
 }
 
-const Content = ({courseparts}) => {
+const Part = (props) => {
+
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+
+}
+
+const Content = ({ courseparts }) => {
     console.log(courseparts)
+    courseparts.map((part => console.log(part.name))) // get all part names
+    return (
+        <div>
+   
+        </div>
+    )
+}
+
+const Total = ({ courseparts }) => {
+    //console.log(courseparts[0].exercises)
 }
 
 const Course = ({ course }) => {
     console.log(course.name)
     return (
         <div>
-            <Header coursename={course.name}/>
-            <Content courseparts={course.parts}/>
+            <Header coursename={course.name} />
+            <Content courseparts={course.parts} />
+            <Total courseparts={course.parts} />
         </div>
     )
 }
