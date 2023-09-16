3e100032ad49544c093d7f2fc349d1a702e965bb
renzyh <nokelainennmarkus@gmail.com>
2023-09-16 08:57:32 +0300

    2.10 step5 done

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
+import PersonForm from './components/PersonForm';
+import Filter from './components/Filter'
+import Persons from './components/Persons';
 
 const App = () => {
-  const App = () => {
-    
-    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456' },
-      { name: 'Ada Lovelace', number: '39-44-5323523' },
-      { name: 'Dan Abramov', number: '12-43-234345' },
-      { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
-  }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
 
+  const [filter, setFilter] = useState('');
   const [newName, setNewName] = useState('');
-  const [newNumber, setnewNumber] = useState('');
-
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -25,7 +25,7 @@ const App = () => {
 
     const normalizedNewName = newName.trim().toLowerCase();
 
-    const personAdded = persons.some((person) => person.name.toLowerCase() === normalizedNewName)
+    const personAdded = persons.some((person) => person.name.toLowerCase() === normalizedNewName);
     const numberAdded = persons.some((person) => person.number === newNumber.trim());
 
     if (personAdded && numberAdded) {
@@ -35,45 +35,40 @@ const App = () => {
 
     setPersons(persons.concat(personObject));
     setNewName('');
-    setnewNumber('');
+    setNewNumber('');
 
     console.log(persons);
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value.trim().toLowerCase());
+  };
+
+  const filteredPersons = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()));
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
   const handleNumberChange = (event) => {
-    setnewNumber(event.target.value);
-  }
+    setNewNumber(event.target.value);
+  };
 
   return (
     <div>
+      
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input />
-      </div>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          <div>
-            number: <input value={newNumber} onChange={handleNumberChange} />
-          </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((person, i) => (
-          <li key={i}>{person.name} {person.number}</li>
-        ))}
-      </ul>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} addPerson={addPerson}/>
+      
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
+
     </div>
   );
+
 };
 
 export default App;
